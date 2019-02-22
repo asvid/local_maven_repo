8efada3ae5b09ff2be69a25a3caae4873b24c692
Adam Świderski <adam.swiderski89@gmail.com>
2019-02-22 09:22:10 +0100

    deps update

--- library/app/src/main/java/io/github/asvid/library/MainActivity.kt
+++ library/app/src/main/java/io/github/asvid/library/MainActivity.kt
@@ -1,16 +1,13 @@
 package io.github.asvid.library
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
 
 }
